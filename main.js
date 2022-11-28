03e772c5e79effb48012a978b6e00ab3a02f6786
7piyushdev <dchlamydia@gmail.com>
2022-11-28 12:37:34 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+var form = document.getElementById("addForm");
+var itemList = document.getElementById("items");
+
+// Form submit event
+form.addEventListener("submit", addItem);
+
+// delete event
+itemList.addEventListener('click', removeItem)
+
+// Add item
+function addItem(e) {
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById("item").value;
+
+  //create new li item
+
+  var li = document.createElement("li");
+  li.className = "list-group-item";
+
+  //Add textNode with input value
+
+  li.appendChild(document.createTextNode(newItem));
+
+  //create the delete button
+
+  var deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+
+  deleteBtn.appendChild(document.createTextNode("X"));
+
+  //append button to li
+  li.appendChild(deleteBtn);
+
+  //append li to list
+  itemList.appendChild(li);
+}
+
+// Remove Item on delete button
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure ?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
